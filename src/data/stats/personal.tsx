ecb3831e18186e630574124b9727497061f3255b
FrancescoMinisini <fg.minisini@gmail.com>
2026-01-03 19:19:15 +0100

    Aggiunte personal stats

--- src/data/stats/personal.tsx
+++ src/data/stats/personal.tsx
@@ -9,7 +9,7 @@ const Age: React.FC = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('2004-07-20T00:10:00');
     setAge(((Date.now() - birthTime.getTime()) / divisor).toFixed(11));
   };
 
@@ -30,16 +30,21 @@ const data: StatData[] = [
     label: 'Current age',
     value: <Age />,
   },
-  {
-    key: 'countries',
-    label: 'Countries visited',
-    value: 53,
-    link: 'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
-  },
+  // {
+  //   key: 'countries',
+  //   label: 'Countries visited',
+  //   value: 53,
+  //   link: 'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+  // },
   {
     key: 'location',
     label: 'Current city',
-    value: 'New York, NY',
+    value: 'Oslo, No',
+  },
+  {
+    key: 'GPA',
+    label: 'GPA',
+    value: '29.5/30',
   },
 ];
 
